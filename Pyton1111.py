a36bccaae630d65c9642936282f4e50ed59d0be9
Your Name <you@example.com>
2020-01-28 11:58:32 +0200

    23232321123

---
+++ Pyton1111.py
@@ -0,0 +1,19 @@
+import random
+a=(random.randint(0,10))
+b=(random.randint(0,10))
+c=(random.randint(0,10))
+print(a,b,c)
+
+if a>b:
+    print("good")
+if a<b:
+    print("bad")
+if a==b:
+    print(str(c) + str("Теперь эта!"))
+    print(c + (a + b))
+    if (a+b)<c:
+        print("very good")
+    if (a+b)>c:
+        print("very bad")
+    if a==b:
+        print("suffer")
